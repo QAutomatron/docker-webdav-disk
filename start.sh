d6fbce2ed6594f803ee2b95f2ebd4f451c283692
Evgenii Skiridov <e.skiridov@corp.mail.ru>
2016-07-15 16:45:08 +0300

    add files

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export WEBDAV_USER
+export WEBDAV_PASSWORD
+export WEBDAV_URL
+
+echo "User is $WEBDAV_USER"
+echo "Password is $WEBDAV_PASSWORD"
+echo "URL is $WEBDAV_URL"
+
+mkdir $HOME/.davfs2
+echo "$WEBDAV_URL $WEBDAV_USER $WEBDAV_PASSWORD" $HOME/.davfs2/secrets
+mkdir -p /mnt/cloud
+mount.davfs $WEBDAV_URL /mnt/cloud
