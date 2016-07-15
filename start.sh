3dad49dfacc4f65014f34471d02c1761c2173a00
Evgenii S <eskiridov@gmail.com>
2016-07-15 17:52:17 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,11 @@ echo "URL is $WEBDAV_URL"
 
 mkdir $HOME/.davfs2
 echo "$WEBDAV_URL $WEBDAV_USER $WEBDAV_PASSWORD" >> $HOME/.davfs2/secrets
-chmod 0700 ~/.davfs2/secrets
+chmod 0700 $HOME/.davfs2/secrets
+
+echo "Secrets file OUTPUT"
+cat $HOME/.davfs2/secrets
+echo "Secrets file OUTPUT END"
 
 mkdir -p /mnt/cloud
 mount.davfs $WEBDAV_URL /mnt/cloud
