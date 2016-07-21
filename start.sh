c2026dcf499098e92b64a575484f901efd9ab930
Evgenii S <eskiridov@gmail.com>
2016-07-21 12:54:38 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,17 +4,15 @@ export WEBDAV_USER
 export WEBDAV_PASSWORD
 export WEBDAV_URL
 
+# print ENV variables
 echo "User is $WEBDAV_USER"
 echo "Password is $WEBDAV_PASSWORD"
 echo "URL is $WEBDAV_URL"
 
-mkdir $HOME/.davfs2
-echo "$WEBDAV_URL $WEBDAV_USER $WEBDAV_PASSWORD" >> $HOME/.davfs2/secrets
-chmod 0700 $HOME/.davfs2/secrets
+# Create entry in secrets file
+echo "$WEBDAV_URL $WEBDAV_USER $WEBDAV_PASSWORD" >> /etc/davfs2/secrets
 
-echo "Secrets file OUTPUT"
-cat $HOME/.davfs2/secrets
-echo "Secrets file OUTPUT END"
-
+# make dir for mount
 mkdir -p /mnt/cloud
+# mount cloud to dir
 mount.davfs $WEBDAV_URL /mnt/cloud
